1ebc8f3dc1ed2bc324e46ad7039f2f5a231f8892
Егор Егор <garry599@yandex.ru>
2021-05-11 07:22:42 +0300

    Исправил отправку сообщений

--- ServerForUnity/Core/User.cs
+++ ServerForUnity/Core/User.cs
@@ -52,7 +52,7 @@ namespace ServerForUnity.Core
         private void AddMessage()
         {
             _server.AddMessage(Message.ToString());
-            _server.BroadcastMessage(UserName, this.Id);
+            _server.BroadcastMessage(UserName + " " + Message.ToString(), this.Id);
         }
 
         public override string GetMessage()
